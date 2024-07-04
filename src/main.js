9703a95c6eded86d3a340886cd871aa364faba95
wang2lengzi <wang2lengzi@88.com>
2024-07-04 09:18:40 +0800

    Feat: 创建 Web 服务器

---
+++ src/main.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+   response.write('hello world!');
+   response.end();
+});
+
+server.listen(3000, () => {
+    console.log('🚀 服务已启动！');
+});
